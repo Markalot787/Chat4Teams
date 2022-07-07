8812bb1ff2d60c0cb7d5efe282df5a2280871f5f
markalot787 <mark_alot@gmail.com>
2022-07-07 17:54:15 -0400

    Added my project

---
+++ backend/connection.js
@@ -0,0 +1,30 @@
+// import { connect } from 'mongoose';
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// mongoose.connect(
+// 	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.qd9ub.mongodb.net/?retryWrites=true&w=majority`,
+// 	() => {
+// 		console.log('connected to mongodb');
+// 	}
+// );
+
+mongoose.connect(
+	process.env.DB,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	() => {
+		console.log('connected to database myDb ;)');
+	}
+);
+
+// mongoose
+// 	.connect(process.env.MONGO_PROD_URI, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 		useCreateIndex: true,
+// 	})
+// 	.then(() => console.log('Database connected!'))
+// 	.catch((err) => console.log(err));
